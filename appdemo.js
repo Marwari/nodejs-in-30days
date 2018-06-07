f8bb2c91e1cdd8978770c051f53d0498d4b4e63b
Marwari <bharattitop@gmail.com>
2018-06-07 21:52:03 +0530

    node+mysql, register form

---
+++ appdemo.js
@@ -0,0 +1,72 @@
+// requires
+var express = require('express');
+var routes = require('routes');
+var http = require('http');
+var path = require('path');
+
+//Including controller/dao for testtable
+var testtable = require('./routes/testtable'); 
+var app = express();
+
+var mysql = require('mysql');
+// all environments
+app.set('port', process.env.PORT || 4302);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+// development only
+
+
+var con = mysql.createConnection({
+		host:"localhost",
+		user:"root",
+		password:"root",
+		database: "demo"
+	});
+app.get('/testtable', testtable.list);
+
+var  url=require('url');
+
+app.get('/register', function(req, res){
+	res.sendFile(__dirname + "/public/register.html");
+
+})	
+
+
+
+app.get('/register_action' , function(req, res){
+	
+	 var  qq=url.parse(req.url,true).query;
+var username=qq.user_name;
+var password=qq.user_password;
+
+con.connect(function(err){
+		if(err) throw err;
+		console.log("Connected!");
+		
+var sql = "insert into login(username,password) values('"+username+"','"+password+"')";
+			  con.query(sql, function (err, result) {
+				if (err) throw err;
+		if(result.length){
+  			res.redirect('/testtable');
+res.end();
+			}
+		else
+		{
+			 res.send("Not  inserted");
+		res.end();
+}
+			  }); 
+	});
+
+
+	
+})	
+
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/tablelist.js
@@ -0,0 +1,21 @@
+var mysql = require('mysql');
+export.list = function(req, res){
+	var con = mysql.createConnnection({
+		host: 'localhost',
+		user: 'root',
+		password: 'root',
+		database: 'demo'
+	});
+	con.connect(function(err){
+		if(err) throw err;
+		console.log('Connected');
+		
+		var sql = "select *from login";
+		con.query(sql, function(err, rows){
+			if(err) throw err;
+			console.log(result[0].rows);
+			
+			res.render('testtable', {page_title:"Test Table", data:rows});
+		});
+	});
+};
