8b2c277da32cfd08ff2e182973ddb909f3e0c70d
Marwari <bharattitop@gmail.com>
2018-06-10 18:30:25 +0530

    index update with mysql

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Auth App' });
 });
+
+var mysql = require('mysql');
+export.list = function(req, res){
+	var con = mysql.createConnnection({
+		host: 'localhost',
+		user: 'root',
+		password: 'root',
+		database: 'demo'
+	});
+	con.connect(function(err){
+		if(err) throw err;
+		console.log('Connected');
+		
+		var sql = "select *from login";
+		con.query(sql, function(err, rows){
+			if(err) throw err;
+			console.log(result[0].rows);
+			
+			res.render('index', {page_title:"Test Table", data:rows});
+		});
+	});
+};
 
 module.exports = router;
